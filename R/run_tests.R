a98c3a8deffe04ca12214a687092468c3527ca6d
Rob Lanfear <rob.lanfear@gmail.com>
2014-02-27 13:10:06 +1100

    initial commit

---
+++ R/run_tests.R
@@ -0,0 +1,5 @@
+library('testthat')
+
+source('p_check.R')
+
+test_dir('tests', reporter = 'Summary')
---
+++ R/tests/test_p_check.r
@@ -0,0 +1,16 @@
+
+# these test check the 
+
+test_that("p_check raises errors for anything that's not a float >=0 and <=0.05", {
+
+	expect_that(p_check(c(0.0)), equals(c(0.0)))
+	expect_that(p_check(0.0), equals(c(0.0)))
+	expect_that(p_check(c(0.05)), equals(c(0.05)))
+	expect_that(p_check(0.05), equals(c(0.05)))
+	expect_that(p_check(c(0.0500001)), throws_error())
+	expect_that(p_check(c(-0.0000001)), throws_error())
+	expect_that(p_check(c(0.0, 0.003, 0.05, 0.05)), equals(c(0.0, 0.003, 0.05, 0.05)))
+	expect_that(p_check(c(0.0, -0.003, 0.05, 0.05)), throws_error())
+	expect_that(p_check('egg'), throws_error())
+
+})
