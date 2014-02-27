761b91fd9b6192642c9a381ac366e501497a89a6
Rob Lanfear <rob.lanfear@gmail.com>
2014-02-27 16:12:04 +1100

    add p_to_pp function
    calculates pp values. This is a bit silly - it's just a multiplication.
    However, I include a check on the initial list of p values, just for
    good measure.

---
+++ R/p_to_pp.R
@@ -0,0 +1,9 @@
+p_to_pp <- function(p) {
+
+	p <- p_check(p)
+
+	pp <- 20.0 * p
+
+	return(pp)
+
+}
--- R/run_tests.R
+++ R/run_tests.R
@@ -2,5 +2,6 @@ library('testthat')
 
 source('binomial_p_test.R')
 source('p_check.R')
+source('p_to_pp.R')
 
 test_dir('tests', reporter = 'Summary')
