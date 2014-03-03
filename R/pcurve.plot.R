1cde06ff1c4070f3f73437d688a6c66298118633
RobertLanfear <rob.lanfear@gmail.com>
2014-03-03 17:43:38 +1100

    better plot
    we use log(count) becuase for many cases we will have a MASSIVE bar at
    0.00-0.01, and then very small bars elsewhere. This makes nicer colours
    for that case.

--- R/pcurve.plot.R
+++ R/pcurve.plot.R
@@ -3,7 +3,7 @@ pcurve.plot <- function(p, binwidth = 0.01) {
 	
 	pl <- ggplot(as.data.frame(p), aes(x=p))
 	pl <- pl + 
-		  geom_histogram(binwidth=binwidth, aes(fill=..count..)) + 
+		  geom_histogram(binwidth=binwidth, aes(fill=log(..count..))) + 
 		  xlim(c(0.0, 0.05))
 	pl
 
