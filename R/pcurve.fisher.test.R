23fb8907431236219b80a6fa6bb6c0b37c883036
RobertLanfear <rob.lanfear@gmail.com>
2014-03-03 17:00:30 +1100

    improve formatting

--- R/pcurve.fisher.test.R
+++ R/pcurve.fisher.test.R
@@ -3,8 +3,6 @@
 # runs fisher's test
 # returns the result
 
-library(MADAM)
-
 pcurve.fisher.test <- function(p) {
 	p <- p.check(p)
 	pp <- p.to.pp(p)
--- R/run_tests.R
+++ R/run_tests.R
@@ -1,9 +1,11 @@
 library('testthat')
+library('MADAM')
 
 source('p.check.R')
 source('limits.check.R')
 source('p.to.pp.R')
 source('pcurve.binomial.test.R')
 source('pcurve.fisher.test.R')
+source('pcurve.plot.R')
 
 test_dir('tests', reporter = 'Summary')
