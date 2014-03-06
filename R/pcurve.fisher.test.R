c2dc5b945848663b5ddb9fe491c9be84b74c8d74
RobertLanfear <rob.lanfear@gmail.com>
2014-03-06 17:04:00 +1100

    documentation for fisher test
    also renamed it, and added two related tests.
    
    Need some unit tests for the new tests, and to fix the test names of
    the old one, and to change the outupt format of all three.

--- R/pcurve.fisher.test.R
+++ R/pcurve.fisher.test.R
@@ -1,11 +1,64 @@
-# A function which takes a vector of p values
-# turns them into pp values
-# runs fisher's test
-# returns the result
+#' Perform a test for right skew p values between 0.00 and 0.05.
+#'
+#' This function uses Fisher's test (implemented in fisher.method
+#' () function of the MADAM package) to ask whether the p values in the 
+#' range 0.00 to 0.05 are more right skewed than you would expect (i.e. 
+#' bunched up towards zero).  
+#' This function replicates an analysis described in Simonsohn et al 
+#' (2013, see references). If you use it, please cite that paper 
+#' and the MADAM package (see references). 
 
-pcurve.fisher.test <- function(p) {
-	p <- p.check(p)
+#' @references Simonsohn, Uri and Nelson, Leif D. and Simmons, Joseph 
+#' P., P-Curve: A Key to the File Drawer (April 24, 2013). Journal of 
+#' Experimental Psychology: General, Forthcoming. 
+#' Available at SSRN: http://ssrn.com/abstract=2256237
+#' @references Kugler KG, Mueller LA, Graber A: MADAM - An Open Source 
+#' Toolbox for Meta-Analysis. Source Code for Biology and Medicine 2010, 5:3
+
+#' @param p a vector of p values between 0.0 and 0.05 (inclusive)
+#' @param ... additional expressions passed to fisher.method() function
+#'   from the MADAM package. 
+
+#' @return This method returns a data frame containing the following columns:
+#' 
+#'   \item{S }{The test statistic from Fisher's test}
+#'   \item{p }{The p value from Fisher's test}
+#'   \item{num.p }{The number of p values used to compute the test statistic}
+#'   \item{df }{The number of degrees of freedom}
+
+#' @keywords Fisher's method, p curve
+#' @export
+#' @seealso \code{\link{binomial.all.test}}
+
+#' @examples
+#' 
+#' p <- c(0.00001, 0.024, 0.002, 0.045, 0.00003, 0.021, 0.0001, 0.0000948, 0.0000002)
+#' fisher.rskew.test(p)
+
+fisher.rskew.test <- function(p) {
 	pp <- p.to.pp(p)
 	r <- fisher.method(matrix(pp, nrow=1))
 	return(r)
+}
+
+
+# NEEDS TESTING
+fisher.lskew.test <- function(p) {
+    p.reversed <- 0.05 - p 
+    pp <- p.to.pp(p.reversed)
+    r <- fisher.method(matrix(pp, nrow=1))
+    return(r)
+}
+
+# NEEDS WRITING
+# Basic idea
+fisher.bias.test <- function(p, limits=c(0.03,0.05)) {
+    limits.check(limits)
+    p <- p.check(p, limits) # removes p's outside limits    
+    p.reversed <- 0.05 - p
+    p.reversed <- p.check(p.reversed)
+
+    pp <- p * (1/(0.05 - limits[1])) # biggest p value gets pp 1
+    r <- fisher.method(matrix(pp, nrow=1))
+    return(r)
 }
